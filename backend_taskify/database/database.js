55a112aee8a2bc5ba348d3512645ed59692bccbf
Anish <111975682+AnishBharati@users.noreply.github.com>
2025-07-14 10:49:20 +0545

    Update database.js

--- backend_taskify/database/database.js
+++ backend_taskify/database/database.js
@@ -1,10 +1,12 @@
+require("dotenv").config(); // Load environment variables from .env
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "Taskify",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
